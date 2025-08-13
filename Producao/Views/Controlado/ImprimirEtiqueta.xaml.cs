2c4477853aaa622d527b7affe39eeaf065b4e0e2
wesley <wog.nascimento@gmail.com>
2025-08-13 13:17:41 -0300

    AJUSTE LAYOUT ETIQUETA

--- Producao/Views/Controlado/ImprimirEtiqueta.xaml.cs
+++ Producao/Views/Controlado/ImprimirEtiqueta.xaml.cs
@@ -211,13 +211,21 @@ namespace Producao.Views.Controlado
                                 SWriter.WriteLine($@"^XA");
                                 SWriter.WriteLine($@"^PW184");
                                 SWriter.WriteLine($@"^CI28");
+                                //SWriter.WriteLine($@"^FT24,313^BQN,2,6");
+                                //SWriter.WriteLine($@"^FH\^FDHA,{etiqueta.barcode}^FS");
+                                //SWriter.WriteLine($@"^FT124,159^AAB,9,5^FH\^FDPRODUTO^FS");
+                                //SWriter.WriteLine($@"^FT139,159^A0B,11,19^FH\^FD{etiqueta.codcompladicional}^FS");
+                                //SWriter.WriteLine($@"^FT124,93^AAB,9,5^FH\^FDETIQUETA^FS");
+                                //SWriter.WriteLine($@"^FT139,93^A0B,11,19^FH\^FD{etiqueta.codigo}^FS");
+                                //SWriter.WriteLine($@"^FT105,160^AAB,9,5^FB121,6,0,C^FH\^FD{etiqueta.descricao_completa}^FS");
+                                //SWriter.WriteLine($@"^PQ1,0,1,Y^XZ");
                                 SWriter.WriteLine($@"^FT24,313^BQN,2,6");
                                 SWriter.WriteLine($@"^FH\^FDHA,{etiqueta.barcode}^FS");
-                                SWriter.WriteLine($@"^FT124,159^AAB,9,5^FH\^FDPRODUTO^FS");
-                                SWriter.WriteLine($@"^FT139,159^A0B,11,19^FH\^FD{etiqueta.codcompladicional}^FS");
-                                SWriter.WriteLine($@"^FT124,93^AAB,9,5^FH\^FDETIQUETA^FS");
-                                SWriter.WriteLine($@"^FT139,93^A0B,11,19^FH\^FD{etiqueta.codigo}^FS");
-                                SWriter.WriteLine($@"^FT105,160^AAB,9,5^FB121,6,0,C^FH\^FD{etiqueta.descricao_completa}^FS");
+                                SWriter.WriteLine($@"^FT160,295^AAB,9,5^FH\^FDPRODUTO^FS");
+                                SWriter.WriteLine($@"^FT175,295^A0B,11,19^FH\^FD{etiqueta.codcompladicional}^FS");
+                                SWriter.WriteLine($@"^FT160,229^AAB,9,5^FH\^FDETIQUETA^FS");
+                                SWriter.WriteLine($@"^FT175,229^A0B,11,19^FH\^FD{etiqueta.codigo}^FS");
+                                SWriter.WriteLine($@"^FT141,160^A0B,15^FB121,8,0,C^FH\^FD{etiqueta.descricao_completa?.Replace("ÚNICO", "")}^FS");
                                 SWriter.WriteLine($@"^PQ1,0,1,Y^XZ");
 
                                 using DatabaseContext db = new();
